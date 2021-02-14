3ab5d12f3fbad7bc9b5a15781f578090589fe269
David Evans <davidje13@users.noreply.github.com>
2021-02-14 18:05:57 +0000

    Tie fractals to specific canvas, fix endianness issue with pallette

--- render-worker.js
+++ render-worker.js
@@ -24,36 +24,68 @@ function makeProjections(sides, scale) {
 	return projections;
 }
 
+function makeAgents(maxAgents, vertices, fraction) {
+	const points = vertices.length / 2;
+	const ifraction = 1.0 - fraction;
+	let agents = new Float32Array(2);
+	agents[0] = vertices[0];
+	agents[1] = vertices[1];
+	let count = 1;
+	while (points >= 2 && count * points < maxAgents) {
+		const next = new Float32Array(count * points * 2);
+		for (let a = 0; a < count; ++ a) {
+			const x = agents[a * 2    ];
+			const y = agents[a * 2 + 1];
+			for (let i = 0; i < points; ++i) {
+				const p = (a * points + i) * 2;
+				next[p    ] = x * ifraction + vertices[i * 2    ] * fraction;
+				next[p + 1] = y * ifraction + vertices[i * 2 + 1] * fraction;
+			}
+		}
+		agents = next;
+		count *= points;
+	}
+	return agents;
+}
+
 const COL_SCALE_NUM = 16384;
 const COL_SCALE = new Uint32Array(COL_SCALE_NUM + 1);
+const COL_SCALE_RGBA = new Uint8ClampedArray(COL_SCALE.buffer);
 for (let i = 0; i < COL_SCALE_NUM; ++ i) {
 	const v = i / (COL_SCALE_NUM - 1);
-	const r = Math.max(0, 255 - Math.floor(Math.pow(v, 0.2) * 256));
-	const g = Math.max(0, 255 - Math.floor(Math.pow(v, 0.5) * 256));
-	const b = Math.max(0, 255 - Math.floor(Math.pow(v, 0.8) * 256));
-	const a = 255;
-	COL_SCALE[i] = (a << 24) | (b << 16) | (g << 8) | r;
+	COL_SCALE_RGBA[i * 4    ] = (1 - Math.pow(v, 0.2)) * 256;
+	COL_SCALE_RGBA[i * 4 + 1] = (1 - Math.pow(v, 0.5)) * 256;
+	COL_SCALE_RGBA[i * 4 + 2] = (1 - Math.pow(v, 0.8)) * 256;
+	COL_SCALE_RGBA[i * 4 + 3] = 255;
 }
-COL_SCALE[0] = 0;
+COL_SCALE_RGBA[3] = 0;
 COL_SCALE[COL_SCALE_NUM] = COL_SCALE[COL_SCALE_NUM - 1];
 
 class Fractal {
-	constructor(points, fraction, size) {
+	constructor(target, points, fraction, { maxAgents = 1000 } = {}) {
+		const size = target.width;
+		this.ctx = target.getContext('2d');
+		this.outData = this.ctx.createImageData(size, size);
+		this.outBuffer = new Uint32Array(this.outData.data.buffer);
+
 		this.points = points;
 		this.fraction = fraction;
 		this.size = size;
 		this.half = Math.floor(size / 2) + 1;
 		this.buckets = new Uint32Array(this.half * size);
 		this.centre = this.half - 0.5;
-		this.projections = makeProjections(points, size * 0.5 - 2);
 		this.vertices = makeVertices(points);
+		this.projections = makeProjections(points, size * 0.5 - 2);
+		this.agents = makeAgents(maxAgents, this.vertices, fraction);
+		this.accumulate(this.agents);
 	}
 
-	accumulate(agents, n) {
+	accumulate(agents) {
 		const { projections, buckets, half, centre } = this;
-		for (let i = 0; i < n; ++ i) {
-			const x = agents[i * 2    ];
-			const y = agents[i * 2 + 1];
+		const A = agents.length;
+		for (let i = 0; i < A; i += 2) {
+			const x = agents[i    ];
+			const y = agents[i + 1];
 			for (let j = 0; j < projections.length; j += 4) {
 				const px = Math.abs(x * projections[j    ] + y * projections[j + 1]) + 0.5;
 				const py =          x * projections[j + 2] + y * projections[j + 3] + centre;
@@ -62,50 +94,21 @@ class Fractal {
 		}
 	}
 
-	spawnAgents(maxAgents) {
-		const { points, vertices, fraction } = this;
-		const ifraction = 1.0 - fraction;
-		if (points < 2) {
-			return;
-		}
-		let agents = new Float32Array(2);
-		agents[0] = vertices[0];
-		agents[1] = vertices[1];
-		let count = 1;
-		this.accumulate(agents, count);
-		while (count * points < maxAgents) {
-			const next = new Float32Array(count * points * 2);
-			for (let a = 0; a < count; ++ a) {
-				const x = agents[a * 2    ];
-				const y = agents[a * 2 + 1];
-				for (let i = 0; i < points; ++i) {
-					const p = (a * points + i) * 2;
-					next[p    ] = x * ifraction + vertices[i * 2    ] * fraction;
-					next[p + 1] = y * ifraction + vertices[i * 2 + 1] * fraction;
-				}
-			}
-			agents = next;
-			count *= points;
-			this.accumulate(agents, count);
-		}
-		this.agents = agents;
-		this.agentCount = count;
-	}
-
 	step(steps) {
-		const { agents, agentCount, points, vertices, fraction } = this;
+		const { agents, points, vertices, fraction } = this;
 		const ifraction = 1.0 - fraction;
+		const A = agents.length;
 		for (let i = 0; i < steps; ++i) {
-			for (let a = 0; a < agentCount; ++ a) {
+			for (let a = 0; a < A; a += 2) {
 				const target = ((Math.random() * points) | 0) * 2;
-				agents[a * 2    ] = agents[a * 2    ] * ifraction + vertices[target    ] * fraction;
-				agents[a * 2 + 1] = agents[a * 2 + 1] * ifraction + vertices[target + 1] * fraction;
+				agents[a    ] = agents[a    ] * ifraction + vertices[target    ] * fraction;
+				agents[a + 1] = agents[a + 1] * ifraction + vertices[target + 1] * fraction;
 			}
-			this.accumulate(agents, agentCount);
+			this.accumulate(agents);
 		}
 	}
 
-	render(target) {
+	render() {
 		const { half, size, buckets } = this;
 		const N = half * size;
 		let max = 0;
@@ -122,7 +125,7 @@ class Fractal {
 			max = Math.max(max, v * 2);
 		}
 		const m = COL_SCALE_NUM / max;
-		const out = new Uint32Array(target.data.buffer);
+		const out = this.outBuffer;
 		for (let y = 0; y < size; ++y) {
 			const p = y * size + half;
 			// pixels on the boundary count double since they would contain their own reflection
@@ -131,17 +134,16 @@ class Fractal {
 				out[p + x] = out[p - x] = COL_SCALE[(buckets[y * half + x] * m) | 0];
 			}
 		}
+		this.ctx.putImageData(this.outData, 0, 0);
 		return [max, sum / (N + size)];
 	}
 }
 
 let out = null;
-let ctx = null;
-let outData = null;
 let fractal = null;
 let change = null;
 let interval = null;
-let lastRatio = null;
+let lastRatio = -1;
 let stopThresh = 0;
 
 self.addEventListener('message', ({ data }) => {
@@ -169,16 +171,18 @@ function stop() {
 
 function step() {
 	if (change) {
-		fractal = new Fractal(Number(change.points) | 0, Number(change.fraction), out.width);
-		ctx = out.getContext('2d');
-		outData = ctx.createImageData(fractal.size, fractal.size);
-		fractal.spawnAgents(change.agents);
+		fractal = new Fractal(
+			out,
+			Number(change.points) | 0,
+			Number(change.fraction),
+			{ maxAgents: change.agents },
+		);
 		change = null;
 		lastRatio = -1;
 		stopThresh = 0;
 	}
 	fractal.step(10);
-	const [max, avg] = fractal.render(outData);
+	const [max, avg] = fractal.render();
 	const ratio = avg / max;
 	if (Math.abs(ratio - lastRatio) < 0.0001) {
 		if ((++stopThresh) > 20) {
@@ -188,5 +192,4 @@ function step() {
 		stopThresh = 0;
 		lastRatio = ratio;
 	}
-	ctx.putImageData(outData, 0, 0);
 }
